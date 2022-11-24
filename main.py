06b1fe4165b9c726f7554cbfb2ea8a73c77e09a5
voxupy <voxu.py@gmail.com>
2022-11-24 18:25:48 +0100

    created body of snake

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from turtle import Screen, Turtle
+import time
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("hello")
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake game")
+screen.tracer(0)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+starting_position = [(0,0),(-20,0),(-40,0)]
+segments = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.penup()
+    new_segment.color('white')
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments)-1,0,-1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+
+
+
+screen.exitonclick()
