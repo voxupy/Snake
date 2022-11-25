7659dc63d4e99810f7ff61a2075bf22c9ca9b613
voxupy <voxu.py@gmail.com>
2022-11-25 09:16:45 +0100

    class food

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 import time
 from snake import Snake
+from food import Food
 
 
 screen = Screen()
@@ -10,6 +11,13 @@ screen.title("Snake game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
