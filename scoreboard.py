12a4daaed5a26d2125770b7282cd9d8fd810abf3
voxupy <voxu.py@gmail.com>
2022-11-29 22:42:15 +0100

    Added highest score, saved to highest_score.txt file.

--- scoreboard.py
+++ scoreboard.py
@@ -4,22 +4,19 @@ FONT = ("Arial",24,"normal")
 ALIGNMENT = "center"
 
 
-
 class Scoreboard(Turtle):
+
     def __init__(self):
-        with open("highest_score.txt") as file:
-            values = file.readlines()
-            values.sort()
+        with open("highest_score.txt") as data:
+            self.highest_score = int(data.read())
 
         super().__init__()
         self.score = 0
-        self.highest_score = int(values[-1])
         self.penup()
         self.goto(0, 265)
         self.color("white")
         self.hideturtle()
         self.update_scoreboard()
-
 
     def update_scoreboard(self):
         self.clear()
@@ -32,7 +29,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highest_score:
             self.highest_score = self.score
-            with open("highest_score.txt", mode='a') as file:
-                file.write(f"\n{self.score}")
+            with open("highest_score.txt", mode='w') as file:
+                file.write(f'{self.highest_score}')
         self.score = 0
         self.update_scoreboard()
